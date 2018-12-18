9d2d36a7dbaa4b1e3ee5a5e041983517b430522a
Alvin Kwong <akwong372@gmail.com>
2018-12-17 19:29:24 -0800

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,15 +1,32 @@
-CREATE DATABASE chat;
+CREATE DATABASE IF NOT EXISTS chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE IF NOT EXISTS messages (
+    ID int NOT NULL AUTO_INCREMENT,
+    MessageText varchar(255),
+    UserID int, 
+    RoomID int, 
+    PRIMARY KEY (ID),
+    FOREIGN KEY (UserID) REFERENCES usernames(ID), 
+    FOREIGN KEY (RoomID) REFERENCES roomnames(ID)
 );
+
+
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE IF NOT EXISTS usernames (
+    ID int NOT NULL AUTO_INCREMENT,
+    UserNames varchar(255),
+    PRIMARY KEY (ID)
+);
 
-
+CREATE TABLE IF NOT EXISTS roomnames (
+    ID int NOT NULL AUTO_INCREMENT,
+    RoomNames varchar(255),
+    PRIMARY KEY (ID)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
