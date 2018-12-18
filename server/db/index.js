efcb7af84fefbb0a828a376192def86f12578966
Alvin Kwong <akwong372@gmail.com>
2018-12-17 21:03:40 -0800

    Working on models and controllers

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,25 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: 'student',
+  database: 'chat'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM messages', function (err, rows, fields) {
+
+  if (err) { console.log(err) }
+
+  console.log('fields here ', fields[0]);
+});
+
+connection.end();
+
+exports.connection = connection;
+
+
 
